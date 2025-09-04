3b65dc8fd3ad673ea74e267596a42b4bd7cd1740
Phyo Htet Paing <phyohtetpaing200303@gmail.com>
2025-09-04 14:54:34 +0700

    Add initial project structure with components, configuration, and dependencies

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,55 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: "10-3-2023",
+      views: 55,
+      author: { _id: 1, name: "Shadow" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://plus.unsplash.com/premium_photo-1665772801542-e9cc7bc60a30?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
+
   return (
     <>
-      <h1 className="text-2xl">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Ptich Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-resultes">No startups found</p>
+          )}
+        </ul>
+      </section>
     </>
   );
 }
